bbc8abd8c607edea8a445af72a78b0994284dc59
yukidaru55 <shiho.2480@gmail.com>
2018-09-24 04:49:19 +0000

    質問を数からテキストに対応させました

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,5 +4,18 @@ class Question < ApplicationRecord
     has_one :answer
     has_many :original_questions
 
-    Question.joins(:answer)
+
+    enum question_number:
+        %i(好きな食べ物なんですか
+            好きな色はなんですか？
+            海派？それとも山派？
+            猫が好きですよね？
+            今日は何の日ですか？
+            私と遊びに行きませんか？
+            好きな映画は何ですか？
+            小学校の思い出を教えてください。
+            尊敬している人は誰ですか？
+            愛読書は何ですか？
+        )
+
 end
